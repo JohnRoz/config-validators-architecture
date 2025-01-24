92bfaaa4c284003ed8b19bd7c14784a7d3d9ee9a
Jonathan <jonathan.rozenblat@gmail.com>
2025-01-24 14:13:41 +0200

    Changed tuple to set

--- src/validators/__init__.py
+++ src/validators/__init__.py
@@ -7,12 +7,12 @@ ValidationFunction = Callable[..., Iterable[BaseValidationError]]
 _T_VALIDATION_FUNCTION = TypeVar("_T_VALIDATION_FUNCTION", bound=ValidationFunction)
 
 
-VALIDATORS_REGISTRY: list[tuple[ValidationFunction, tuple[type[BaseConfig], ...]]] = []
+VALIDATORS_REGISTRY: list[tuple[ValidationFunction, set[type[BaseConfig]]]] = []
 
 
 def register_validator(*config_types):
     def decorator(func: _T_VALIDATION_FUNCTION) -> _T_VALIDATION_FUNCTION:
-        VALIDATORS_REGISTRY.append((func, config_types))
+        VALIDATORS_REGISTRY.append((func, set(config_types)))
         return func
 
     return decorator
