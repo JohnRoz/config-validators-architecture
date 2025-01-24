0bb75017bd01555f8d57182949c5e1f192c0f8ea
Jonathan <jonathan.rozenblat@gmail.com>
2025-01-24 18:52:20 +0200

    Removed redundant example

--- src/models/my_new_thing_config.py
+++
@@ -1,9 +0,0 @@
-from .. import config_loader
-from .base_config import BaseConfig
-
-
-@config_loader.register_config_model(filename="MyNewThingConfig.json")
-class MyNewThingConfig(BaseConfig):
-    name: str
-    statuses: list[str]
-    ...
