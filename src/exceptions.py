c54a542b30ae37a9040e49cde902a09ba0cfe327
Jonathan <jonathan.rozenblat@gmail.com>
2025-01-24 18:39:31 +0200

    Added more exceptions and moved the str implementation to the base

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,2 +1,24 @@
+from typing import Any
+
+
 class BaseException(Exception):
-    pass
+    error_msg: str
+    _error_kwargs: dict[str, Any]
+
+    def __init__(self, err_msg: str, **kwargs):
+        super().__init__(err_msg)
+
+        self.error_msg = err_msg
+        self._error_kwargs = kwargs
+
+    def __str__(self) -> str:
+        _NEW_LINE = "\n"
+        return (
+            f"ERROR: "
+            + self.error_msg
+            + _NEW_LINE
+            + _NEW_LINE.join(f"{kwarg_name}={str(kwarg_value)}" for kwarg_name, kwarg_value in self._error_kwargs.items())
+        )
+
+    def __repr__(self) -> str:
+        return str(self)
---
+++ src/models/exceptions.py
@@ -0,0 +1,5 @@
+from ..exceptions import BaseException
+
+
+class ConfigCreationFailedError(BaseException):
+    pass
