7142bb9f500dcd4a8ec40e5bc5bade0dd6ea7d08
Jonathan <jonathan.rozenblat@gmail.com>
2025-01-24 17:04:46 +0200

    Fixed bug in the tests utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 import os
 
-TESTS_DIR = os.path.basename(__file__)
+TESTS_DIR = os.path.dirname(__file__)
 TEST_RESOURCES_DIR = os.path.join(TESTS_DIR, "test_resources")
 
 VALID_CONFIGS_PATH = os.path.join(TEST_RESOURCES_DIR, "valid_configs")
