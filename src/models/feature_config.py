9fe599311cceabc661ec5e8e06c768e339393de2
Jonathan <jonathan.rozenblat@gmail.com>
2025-01-24 13:12:36 +0200

    Made the models more complicated to be more realistic

--- src/models/feature_config.py
+++ src/models/feature_config.py
@@ -3,8 +3,12 @@ from pydantic import BaseModel
 from .. import config_loader
 
 
-@config_loader.register_config_model(filename="FeatureConfig.json")
-class FeatureConfig(BaseModel):
+class SingleFeatureConfig(BaseModel):
     name: str
     subfeature_names: list[str]
     ...
+
+
+@config_loader.register_config_model(filename="FeatureConfig.json")
+class FeatureConfig(BaseModel):
+    features: list[SingleFeatureConfig]
--- src/models/subfeature_config.py
+++ src/models/subfeature_config.py
@@ -3,7 +3,11 @@ from pydantic import BaseModel
 from .. import config_loader
 
 
-@config_loader.register_config_model(filename="SubFeatureConfig.json")
-class SubFeatureConfig(BaseModel):
+class SingleSubFeatureConfig(BaseModel):
     name: str
     ...
+
+
+@config_loader.register_config_model(filename="SubFeatureConfig.json")
+class SubFeatureConfig(BaseModel):
+    subfeatures: list[SingleSubFeatureConfig]
