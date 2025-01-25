30de25ab4883b8014179e2c97413b84c58f3a947
Jonathan <jonathan.rozenblat@gmail.com>
2025-01-25 18:19:39 +0200

    Added main example of how to call the validations & config loader with the raise exceptions flag

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from src.config_loader import ConfigLoader
 from src.cross_config_validations import cross_config_validations_runner
+from src.exceptions import BaseValidationError
 
 # Example main:
 
 
-def main():
-    configs_dir = "./tests/test_resources/invalid_configs"
+def main_without_raising():
+    configs_dir = "./tests/test_resources/cross_configs/invalid_configs"
     loader = ConfigLoader.load(configs_dir)
 
     for err in loader.config_creation_errors:
@@ -20,5 +21,20 @@ def main():
         print("All validations passed!")
 
 
+def main_with_raising():
+    configs_dir = "./tests/test_resources/cross_configs/invalid_configs"
+    try:
+        loader = ConfigLoader.load(configs_dir, should_raise_on_error=True)
+        cross_config_validations_runner.run_validations(loader.configs, should_raise_on_error=True)
+    except* BaseValidationError as e:
+        for validation_err in e.exceptions:
+            print("Caught a validation error:\n", validation_err)
+    except* BaseException as ex_group:
+        for err in ex_group.exceptions:
+            print(err)
+    else:
+        print("All validations passed!")
+
+
 if __name__ == "__main__":
-    main()
+    main_with_raising()
